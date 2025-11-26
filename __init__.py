a3f91d3b022aca849f4450e44a9b9bef79f58496
Asutosh Khairnar <90890558+asutosh8460@users.noreply.github.com>
2025-11-26 13:43:09 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from .routes import comment_routes
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret'
-    app.register_blueprint(comment_routes)
-    return app
